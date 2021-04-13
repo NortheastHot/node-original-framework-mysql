2e10c695b855f7f18a44617339d2fe41d4bfb7e4
qiheng <aiguozhex@163.com>
2021-04-13 21:26:28 +0800

    v1   /api/blog/list

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,14 +1,17 @@
+const {exec} = require('../db/mysql')
+
 const getList = (author, keyword) => {
-    // 先返回假数据（格式是正确的）
-    return [
-        {
-            id: 1,
-            title: '标题1',
-            content: '内容1',
-            createTime: 1618113001142,
-            author: 'kizi'
-        }
-    ]
+    let sql = `select * from blogs where 1=1 `
+    if(author){
+        sql += `and author='${author}' `
+    }
+    if(keyword){
+        sql += `and title like '%${keyword}%' `
+    }
+    sql += `order by createtime desc;`
+
+    // 返回promise
+    return exec(sql)
 }
 
 const newBlog = (blogData ={}) => {
